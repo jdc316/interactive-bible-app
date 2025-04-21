4fd841cf4019353dcd80851b4b00ab792d6a6f7f
Josh <jdc316@gmail.com>
2025-04-21 19:46:08 -0400

    split into front and back end folders

---
+++ front-end/interactive-bible-app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'interactive-bible-app',
+  webDir: 'www'
+};
+
+export default config;
